986190796a7d68527f4419a3e417bd3904f9ef48
diskriminant <diskriminant.was.taken@gmail.com>
2022-08-30 21:43:13 +0400

    End gate teleport

---
+++ src/main/java/com/diskriminant/ahk/mixin/EndGatewayBlockEntityMixin.java
@@ -0,0 +1,20 @@
+package com.diskriminant.ahk.mixin;
+
+import com.diskriminant.ahk.AchievementHardcoreMod;
+import net.minecraft.block.entity.EndGatewayBlockEntity;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EndGatewayBlockEntity.class)
+public class EndGatewayBlockEntityMixin {
+
+    @Inject(method = "canTeleport", at = @At("HEAD"), cancellable = true)
+    private static void canTeleportInject(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (!AchievementHardcoreMod.isAllowTeleportViaEndGate) {
+            cir.setReturnValue(false);
+        }
+    }
+}
