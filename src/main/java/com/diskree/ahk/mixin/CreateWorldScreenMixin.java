09e552cccf179bb0693a620f594dc480e322bc48
diskree <diskriminant.was.taken@gmail.com>
2022-10-28 19:57:39 +0400

    Lock datapacks button on world creation

--- src/main/java/com/diskree/ahk/mixin/CreateWorldScreenMixin.java
+++ src/main/java/com/diskree/ahk/mixin/CreateWorldScreenMixin.java
@@ -1,6 +1,7 @@
 package com.diskree.ahk.mixin;
 
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.resource.DataPackSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,7 +18,11 @@ public class CreateWorldScreenMixin {
     @Shadow
     public boolean hardcore;
 
-    @Shadow protected DataPackSettings dataPackSettings;
+    @Shadow
+    protected DataPackSettings dataPackSettings;
+
+    @Shadow
+    private ButtonWidget dataPacksButton;
 
     @Inject(method = "createLevel", at = @At("HEAD"))
     public void applyDataPacksInject(CallbackInfo ci) {
@@ -27,4 +32,10 @@ public class CreateWorldScreenMixin {
             dataPackSettings = new DataPackSettings(enabled, new ArrayList<>());
         }
     }
+
+    @Inject(method = "init", at = @At("RETURN"))
+    public void initInject(CallbackInfo ci) {
+        dataPacksButton.active = false;
+    }
+
 }
